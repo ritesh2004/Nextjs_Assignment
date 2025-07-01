045a40ac966565431645c03080c8080b142234dd
Ritesh Pramanik <pramanikritesh907@gmail.com>
2025-07-01 12:10:23 +0530

    hero background color updated

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,14 @@ import HeroSection from "../components/HeroSection";
 export default function Home() {
   return (
     <div>
-      <div className="my-10 w-[90%] mx-auto">
-        <span className="text-2xl"> <span className="text-4xl text-[#118ab2]">D</span>r. <span className="text-4xl text-[#118ab2]">S</span>erena <span className="text-4xl text-[#118ab2]">B</span>lake</span>
-        <br />
-        <span className="text-2xl text-[#118ab2]">Clinical Psychologist</span>
+      <div className="overflow-hidden bg-hero-background">
+        <div className="my-10 w-[90%] mx-auto">
+          <span className="text-2xl"> <span className="text-4xl text-[#118ab2]">D</span>r. <span className="text-4xl text-[#118ab2]">S</span>erena <span className="text-4xl text-[#118ab2]">B</span>lake</span>
+          <br />
+          <span className="text-2xl text-[#118ab2]">Clinical Psychologist</span>
+        </div>
+        <HeroSection />
       </div>
-      <HeroSection />
     </div>
   );
 }
